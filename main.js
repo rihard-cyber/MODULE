f0ec5a5ffd23cec14da60e566a48129dff05ecf4
rihard-cyber <richardpl.meha@gmail.com>
2023-04-20 15:28:38 +0700

    commit origin

---
+++ main.js
@@ -0,0 +1,3 @@
+import data from "/user.js";
+
+console.log(data);
---
+++ user.js
@@ -0,0 +1,10 @@
+const data = [
+  { name: "M iqbal", email: "Iqbal96@gmail.com", number: "081243844889" },
+  { name: "Andika", email: "dika@gmail.com", number: "081243844889" },
+  { name: "Maryono", email: "dema@gmail.com", number: "081243844889" },
+  { name: "Aris", email: "ris@gmail.com", number: "081243844889" },
+  { name: "Richard", email: "richard@gmail.com", number: "081243844889" },
+  { name: "novita", email: "novi@gmail.com", number: "081243844889" },
+];
+
+export default data;
